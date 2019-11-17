71022e352bdb98bbb85d6300099c69574ff819da
Johan Ospina Nunez <Aospina@intergrupo.com>
2019-11-17 13:47:17 -0500

    primer subida

---
+++ ConnectionService/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using EmployeeBL;
+using EmployeeBL.Abstracts;
+
+namespace ConnectionService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var app = new ContainerBuilder();
+            app.RegisterType<Employee>().As<IEmployee>();
+            var builder = app.Build();
+            var _employee = builder.Resolve<IEmployee>();
+            var test = _employee.CreateProjectModified();
+            return View(test);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EmployeeBL/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+using EmployeeBL.Abstracts;
+using Newtonsoft.Json.Linq;
+
+
+namespace EmployeeBL
+{
+    public class Employee : IEmployee
+    {
+
+        public IList<Employees> CreateProjectModified()
+        {
+            string baseUrl = "http://masglobaltestapi.azurewebsites.net/api/Employees";
+
+            var json = new WebClient().DownloadString(baseUrl);
+
+            JArray PersonArray = JArray.Parse(json);
+
+            IList<Employees> persona = PersonArray.Select(p => new Employees
+            {
+                name = (string)p["name"],
+                contractTypeName = (string)p["contractTypeName"],
+                roleId = (string)p["roleId"],
+                roleName = (string)p["roleName"],
+                roleDescription = (string)p["roleDescription"],
+                hourlySalary = (int)p["hourlySalary"],
+                monthlySalary = (int)p["monthlySalary"]
+
+            }).ToList();
+
+            foreach (var registro in persona)
+            {
+                if (registro.contractTypeName == "HourlySalaryEmployee")
+                {
+                    registro.Total = 120 * registro.hourlySalary * 12;
+                }
+                else
+                {
+                    registro.Total = registro.monthlySalary * 12;
+                }
+            }
+
+            return persona;
+        }
+
+
+    }
+}
